3118737175e4f966bb65c11375ec718afdc1d463
Peter Brown <phbrown@acm.org>
2026-01-05 13:51:22 -0500

    End of class 1/5 PM

---
+++ CtoF.py
@@ -0,0 +1,12 @@
+def main(args: list[str]) -> int:
+    # Read the Celsius temperature from the keyboard
+    degC: float = float(input("Please enter a temperature in Celsius: "))
+    # Convert it to Fahrenheit
+    degF: float = degC * 1.8 + 32
+    # Print out the Fahrenheit temperature
+    print(degC, 'degrees Celsius is', degF, 'degrees Fahrenheit')
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
