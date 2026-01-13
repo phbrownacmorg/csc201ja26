3d1173b00e8397dcd98752aaa0645a3fbe84b41f
Peter Brown <phbrown@acm.org>
2026-01-13 13:25:15 -0500

    Quadratic with functions

--- quadratic.py
+++ quadratic.py
@@ -1,21 +1,32 @@
 import math # Tells Python we're going to use functions from the math library
 
-def main(args: list[str]) -> int:
-    """Program to find the roots of a quadratic system."""
-    # Get inputs (a, b, and c define the system)
+def readConstants() -> tuple[float, float, float]:
+    """Read and return the constants a, b, and c of a quadratic system from the keyboard."""
     print('Please enter the constants a, b, and c for a quadratic ax**2 + bx + c = 0')
     a: float = float(input('a: '))
     b: float = float(input('b: '))
     c: float = float(input('c: '))
-    print('The system is ', a, '*x**2 + ', b, '*x + ', c, ' = 0', sep="")
+    return a, b, c
 
-    # Find the roots, if the system has real roots
-    determinant: float = math.pow(b,2) - 4*a*c
+def determinant(a: float, b: float, c: float) -> float:
+    return math.pow(b,2) - 4*a*c
+
+def findRoots(a: float, b: float, c: float) -> tuple[float, float]:
+    """Find the roots of the quadratic system A*x**2 + B*x + C = 0, if the
+        system has real roots."""
+    det = determinant(a, b, c)
     # Negative determinant causes a crash right here
-    det_root = math.sqrt(determinant)
+    det_root = math.sqrt(det)
     root1 = (-b + det_root) / (2*a)
     root2 = (-b - det_root) / (2*a)
-    # Print the root(s)
+    return root1, root2
+
+def main(args: list[str]) -> int:
+    """Program to find the roots of a quadratic system."""
+    a, b, c = readConstants()
+    print('The system is ', a, '*x**2 + ', b, '*x + ', c, ' = 0', sep="")
+
+    root1, root2 = findRoots(a, b, c)
     print('The roots of the system are', root1, 'and', root2)
     return 0
 
