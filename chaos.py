569ffa95955a4c96c8c20ed215fc1088f389cb50
Peter Brown <phbrown@acm.org>
2026-01-22 11:55:10 -0500

    FIles

--- chaos.py
+++ chaos.py
@@ -1,15 +1,66 @@
-import math
+def printChaosTable(x: float, n: int) -> None:
+    print(f'{'n':^3}\t{'x':^12}')
+    print('-' * 20)
+    print(f'{'0':>2}\t{x:0.10f}')
+    for i in range(n): # type: ignore
+        x = 3.9 * x * (1 - x)
+        print(f'{i+1:>2}\t{x:0.10f}')
+    print()
+
+def writeChaosTable(outfilename: str, x: float, n:int) -> None:
+    with open(outfilename, 'a') as f:
+        f.write(f'{'n':^3}\t{'x':^12}\n')
+        f.write('-' * 20 + '\n')
+        f.write(f'{'0':>2}\t{x:0.10f}\n')
+        for i in range(n): # type: ignore
+            x = 3.9 * x * (1 - x)
+            f.write(f'{i+1:>2}\t{x:0.10f}\n')
+        f.write('\n')
+
+def runInteractively() -> None:
+    try:
+        while True:
+            x = float(input("Enter a number between 0 and 1: "))
+            #print('Initial x:', x)
+            n = int(input('Enter the number of iterations: '))
+            printChaosTable(x, n)
+    except ValueError:
+        pass
+
+def runFromFiles(infilename: str, outfilename: str = '') -> None:
+    with open(infilename) as f:
+        linenum = 0
+        for line in f.readlines(): # One scenario per line
+            linenum = linenum + 1
+            errmsg = f'Error on line {linenum}: '
+            numbers = line.split(',')
+            if len(numbers) != 2:
+                print(errmsg + 'line does not have two numbers')
+                continue
+            try:
+                x = float(numbers[0])
+                n = int(numbers[1])
+                if not (0 < x < 1):
+                    raise ValueError('value of x is outside the range (0, 1).')
+                elif n < 0:
+                    raise ValueError('value of n cannot be negative.')
+            except ValueError as e:
+                if e.args[0].startswith('value of '):
+                    print(errmsg + e.args[0])
+                else:
+                    print(errmsg + 'number cannot be read.')
+                continue
+            else:
+                if len(outfilename) == 0:
+                    printChaosTable(x, n)
+                else:
+                    writeChaosTable(outfilename, x, n)
+
 
 def main(args: list[str]) -> int:
     print("This program illustrates a chaotic function")
-    x = float(input("Enter a number between 0 and 1: "))
-    # Clamp x to the range (0, 1)
-    x = max(min(x, 1 - math.ulp(1)), 0 + math.ulp(0))
-    print('Initial x:', x)
-
-    for i in range(10): # type: ignore
-        x = 3.9 * x * (1 - x)
-        print(f'{x:0.10f}')
+    #runInteractively()
+    runFromFiles('chaos-seeds.csv', 'chaos-out.txt')
     return 0
 
 if __name__ == '__main__':
--- idmaker.py
+++ idmaker.py
@@ -1,15 +1,27 @@
-def make_userid(name: str) -> str:
-    """Given a NAME in last-name-first format, create and return a 
-        Converse-style user ID."""
+from csv import DictReader, DictWriter
+
+def name_to_dict(name: str) -> dict[str, str]:
     # Split off the last name
     parts = name.split(',') # Note there may not be just two items in the list
     lastname = parts[0]
     othernames = parts[-1]
+    return {'lastname': lastname, 'othernames': othernames}
+
+def make_userid(namedict: dict[str, str]) -> str:
+    """Given a NAME in last-name-first format, create and return a 
+        Converse-style user ID."""
+    lastname = namedict['lastname']
+    # If the name contains a comma, get rid of tha part from the comma to the end
+    if ',' in lastname:
+        lastname = lastname[:lastname.find(',')]
+
+    othernames = namedict['othernames']
 
     # Clear the last name of characters we don't want to include
-    badchars = " '"
-    for c in badchars:
-        lastname = lastname.replace(c, '')
+    goodchars = '-'
+    for c in lastname:
+        if not c.isalpha() and c not in goodchars:
+            lastname = lastname.replace(c, '')
 
     # Get the first two initials
     parts = othernames.split()
@@ -21,10 +33,27 @@ def make_userid(name: str) -> str:
     id = (initials + lastname + '001').lower()
     return id
 
+def runFromFiles(infile: str, outfile: str) -> None:
+    names: list[dict[str, str]] = []
+    with open(infile, 'r') as f:
+        reader = DictReader(f)
+        for row in reader:
+            userdict = row.copy()
+            userdict['userid'] = make_userid(row)
+            names.append(userdict)
+
+    with open(outfile, 'w') as f:
+        writer = DictWriter(f, names[0].keys())
+        writer.writeheader()
+        writer.writerows(names)
+
 def main(args: list[str]) -> int:
-    raw_name = input('Please enter a full name, last name first, separated by a comma: ')
-    print(f'The user id for the name "{raw_name}" is', end=' ')
-    print(f'"{make_userid(raw_name)}"')
+    infilename = input('Please enter the name of the CSV file to read from: ')
+    outfilename = input('Please enter the name of the CSV file to output to: ')
+    runFromFiles(infilename, outfilename)
+    # raw_name = input('Please enter a full name, last name first, separated by a comma: ')
+    # print(f'The user id for the name "{raw_name}" is', end=' ')
+    # print(f'"{make_userid(name_to_dict(raw_name))}"')
     return 0
 
 if __name__ == '__main__':
