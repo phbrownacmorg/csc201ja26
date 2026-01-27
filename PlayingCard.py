d7fa57abb5d6a4325ad460f3f058cab3ebb0685e
Peter Brown <phbrown@cs.unc.edu>
2026-01-27 15:07:40 -0500

    PlayingCard

---
+++ PlayingCard.py
@@ -0,0 +1,61 @@
+# CONVENTION: Classes are defined in a file that has the same name as the class.
+
+class PlayingCard: # CONVENTION: Class names are in CamelCase, where the first
+    # letter of each word is capitalized.
+    """Class to represent a playing card of the traditional French pattern, as
+        commonly used in English-speaking countries to play blackjack, poker,
+        solitaire, hearts, war, and a whole host of other card games.  A 
+        PlayingCard is immutable once created."""
+    
+    # CONVENTION: Constants, especially class-level constants, have names in ALL_CAPS.
+    SUITS = ('clubs', 'diamonds', 'hearts', 'spades')
+    RANKS = tuple([str(i) for i in range(2, 11)]) + ('jack', 'queen', 'king', 'ace')
+
+    # Constructor
+    def __init__(self, rank: str, suit: str) -> None:
+        """Construct a new PlayingCard of the given RANK and SUIT."""
+        if rank not in self.RANKS:
+            raise ValueError(f'Invalid rank: {rank}')
+        elif suit not in self.SUITS:
+            raise ValueError(f'Invalid suit: {suit}')
+        self._rank = rank
+        self._suit = suit
+    
+    # ----------- Accessor methods -------------------
+
+    def suit(self) -> str:
+        return self._suit
+    
+    def rank(self) -> str:
+        return self._rank
+    
+    def __str__(self) -> str:
+        return self.rank() + ' of ' + self.suit()
+
+    def __eq__(self, other: object) -> bool:
+        result = True
+        if not hasattr(other, 'rank') or self.rank() != other.rank(): ## type: ignore
+            result = False
+        elif not hasattr(other, 'suit') or self.suit() != other.suit(): ## type: ignore
+            result = False
+        return result
+    
+    # Primarily for sorting
+    def __lt__(self, other: 'PlayingCard') -> bool:
+        try:
+            result = (self.RANKS.index(self.rank()) < self.RANKS.index(other.rank()))
+            if self.rank() == other.rank():
+                result = (self.SUITS.index(self.suit()) < self.SUITS.index(other.suit()))
+        except IndexError: # other.rank() isn't in self.RANKS or other.suit() isn't in self.SUITS
+            raise ValueError(f'Incompatible ranks and suits: {self}, {other}')
+        return result
+
+    # ----------- Class method ----------------------
+
+    @classmethod
+    def makeDeck(cls) -> list['PlayingCard']:
+        deck: list[PlayingCard] = []
+        for rank in cls.RANKS:
+            for suit in cls.SUITS:
+                deck.append(PlayingCard(rank, suit))
+        return deck
---
+++ TestPlayingCard.py
@@ -0,0 +1,71 @@
+import unittest
+# import the code you want to test here
+from PlayingCard import PlayingCard
+
+class TestNothing(unittest.TestCase):
+
+    # Every method that starts with the string "test"
+    # will be executed as a unit test
+    def testSuitAccessor(self) -> None:
+        for suit in PlayingCard.SUITS:
+            with self.subTest(suit=suit):
+                self.assertEqual(PlayingCard('5', suit).suit(), suit)
+
+    def testRankAccessor(self) -> None:
+        for rank in PlayingCard.RANKS:
+            with self.subTest(rank=rank):
+                self.assertEqual(PlayingCard(rank, 'hearts').rank(), rank)
+
+    def testConstructorBadRank(self) -> None:
+        with self.assertRaises(ValueError) as cm:
+            PlayingCard('scullion', 'diamonds')
+        self.assertEqual(cm.exception.args[0], 'Invalid rank: scullion')
+
+    def testConstructorBadSuit(self) -> None:
+        with self.assertRaises(ValueError) as cm:
+            PlayingCard('ace', 'cockroaches')
+        self.assertEqual(cm.exception.args[0], 'Invalid suit: cockroaches')
+
+    def testStr(self) -> None:
+        for rank in PlayingCard.RANKS:
+            for suit in PlayingCard.SUITS:
+                with self.subTest(rank=rank, suit=suit):
+                    print(PlayingCard(rank, suit))
+                    self.assertEqual(str(PlayingCard(rank, suit)), f'{rank} of {suit}')
+
+    def testEq(self) -> None:
+        for rank1 in PlayingCard.RANKS:
+            for suit1 in PlayingCard.SUITS:
+                card1 = PlayingCard(rank1, suit1)
+                for rank2 in PlayingCard.RANKS:
+                    for suit2 in PlayingCard.SUITS:
+                        card2 = PlayingCard(rank2, suit2)
+                        with self.subTest(rank1=rank1, suit1=suit1, rank2=rank2, suit2=suit2):
+                            self.assertEqual(card1 == card2, rank1 == rank2 and suit1 == suit2)
+
+    def testLt(self) -> None:
+        for rank1 in PlayingCard.RANKS:
+            for suit1 in PlayingCard.SUITS:
+                card1 = PlayingCard(rank1, suit1)
+                for rank2 in PlayingCard.RANKS:
+                    for suit2 in PlayingCard.SUITS:
+                        card2 = PlayingCard(rank2, suit2)
+                        with self.subTest(rank1=rank1, suit1=suit1, rank2=rank2, suit2=suit2):
+                            rankIdx1 = PlayingCard.RANKS.index(rank1)
+                            rankIdx2 = PlayingCard.RANKS.index(rank2)
+                            suitIdx1 = PlayingCard.SUITS.index(suit1)
+                            suitIdx2 = PlayingCard.SUITS.index(suit2)
+                            self.assertEqual(card1 < card2,
+                                             (rankIdx1 < rankIdx2) or (rank1 == rank2 and suitIdx1 < suitIdx2))
+
+    def testMakeDeck(self) -> None:
+        deck = PlayingCard.makeDeck()
+        self.assertEqual(len(deck), 52)
+        for rank in PlayingCard.RANKS:
+            for suit in PlayingCard.SUITS:
+                with self.subTest(rank=rank, suit=suit):
+                    self.assertEqual(deck.count(PlayingCard(rank, suit)), 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
